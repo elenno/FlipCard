3d2eaf38ffb3a44c5dd731a51b72d8bef33e0a79
elenno <elenno.chen@gmail.com>
2016-01-02 10:38:16 +0800

    //DataMgr采用静态的方式

--- src/Block.ts
+++ src/Block.ts
@@ -15,9 +15,23 @@ class Block extends egret.Bitmap{
         this.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onTouchTap,this);
 	}
 	
-	public onTouchTap(){
+	public onTouchTap(evt:egret.Event){
         this.texture = RES.getRes(this.m_sResName);
         this.touchEnabled = false;
+        var ret = DataMgr.onBlockTouched(this.m_nIdx);
+        if (ret == RESULT.FINISH){
+            this.dispatchEventWith("finish");
+        }
+        else if(ret == RESULT.BINGO){
+            this.dispatchEventWith("bingo");
+        }
+        else if (ret == RESULT.WRONG){
+            this.dispatchEventWith("wrong");
+        }
+        else if (ret == RESULT.FIRST){
+            this.dispatchEventWith("first");
+        }
+        
 	}
 	
 	public init():void{
--- src/DataMgr.ts
+++ src/DataMgr.ts
@@ -3,11 +3,64 @@
  * @author 
  *
  */
+enum RESULT {
+    BINGO = 0,
+    FIRST = 1,
+    WRONG = 2,
+    FINISH = 3
+}
 class DataMgr {
-    
+       
     public m_nGameStatus: number;
     public m_Timer: egret.Timer;
+    public static _firstIdx: number = -1;
+    public static _secondIdx: number = -1;
+    public static _lastIdx: number = -1;
+    public static _idArray: number[];
 	public constructor() {
 	    
+	}
+	
+	public static init():void {
+        DataMgr._idArray = new Array<number>(20);
+        for(var i = 0;i < 20; i++){
+            DataMgr._idArray[i] = 0;
+        }
+	}
+	
+	public static isGameFinish(): Boolean{
+        for(var i = 0, len = DataMgr._idArray.length; i < len; i++){
+            if(DataMgr._idArray[i] != -1)
+                return false;
+        }
+        return true;
+	}
+	
+	public static onBlockTouched(idx):number{
+    	  if(DataMgr._lastIdx == -1) {
+            //first one 
+            DataMgr._lastIdx = idx;
+            return RESULT.FIRST;
+        }
+	    else {
+            DataMgr._firstIdx = DataMgr._lastIdx;
+            DataMgr._secondIdx = idx;
+            DataMgr._lastIdx = -1; 
+            if(DataMgr._idArray[DataMgr._firstIdx] == DataMgr._idArray[DataMgr._secondIdx]) {
+                DataMgr._idArray[DataMgr._firstIdx] = -1;
+                DataMgr._idArray[DataMgr._secondIdx] = -1;
+                //check is finish
+                if(DataMgr.isGameFinish()) {
+                    return RESULT.FINISH;
+                }
+                return RESULT.BINGO;
+            }
+            else{
+                //wrong
+                return RESULT.WRONG;
+            } 
+        }
+        
+        return 0;
 	}
 }
